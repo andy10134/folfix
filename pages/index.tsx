75e1c1dc03c7ccbfe317ca550472daa6f7965148
Andy Molina <andy10134@hotmail.com>
2023-11-03 08:17:36 -0300

    commit del pull request

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,5 +5,7 @@ export default function Page() {
     return <div className="container mx-auto flex flex-col items-center md:items-start gap-14">
       <Navbar></Navbar>
       <MovieSelection></MovieSelection>
+      <MovieSelection></MovieSelection>
+      <MovieSelection></MovieSelection>
     </div>
   }
